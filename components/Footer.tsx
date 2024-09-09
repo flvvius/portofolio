866a4aec5b6533679f9032fe693dbe5f11f150ef
flavius <flaviuscojocaru19@gmail.com>
2024-09-09 22:12:03 +0300

    type fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,7 @@ const Footer = () => {
               key={profile.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <img src={profile.img} alt={profile.img} width={20} height={20} />
             </a>
           ))}
         </div>
