9f3f08fe44bc7b3d831ea392fee6a394815a229a
flavius <flaviuscojocaru19@gmail.com>
2025-03-04 14:35:16 +0200

    description

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Flavius's Portfolio",
-  description: "Freelancer, Web Developer, and Software Engineer",
+  description: "passionate Web Developer",
 };
 
 export default function RootLayout({
